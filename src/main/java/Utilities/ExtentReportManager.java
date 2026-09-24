2ae6978e4b02ba1267339dd2ba84fb05f451a17e
Akash Kumar <akashkumar14121993@gmail.com>
2026-09-24 17:42:18 +0530

    CPOV script updated on 24th Sep 2026 after SMOKE TESTING

--- src/main/java/Utilities/ExtentReportManager.java
+++ src/main/java/Utilities/ExtentReportManager.java
@@ -66,7 +66,7 @@ public class ExtentReportManager implements ITestListener
         // Set system information
         extent.setSystemInfo("Application", "CPOV");
 
-        extent.setSystemInfo("Module", "Chrysler");
+        extent.setSystemInfo("Module", "RAM");
 
         extent.setSystemInfo("Environment", "Production");
 
